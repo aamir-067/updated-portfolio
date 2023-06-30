1a7e7e204395ddf9a23cea6800377e11dddbe425
aamir <aamirkhan@engineer.com>
2023-06-30 16:52:48 +0500

    first commit completed till 2nd page not responsive

---
+++ script.js
@@ -0,0 +1,31 @@
+// // for random quote and auther
+
+function updateQuote(quotes){
+    const randomIndex = Math.floor(Math.random() * 1642);
+    const randomQuote = quotes[randomIndex];
+    const quoteTextElement = document.getElementById('quoteText');
+    const quoteAuthorElement = document.getElementById('quoteAuthor');
+    if (quoteTextElement && quoteAuthorElement) {
+    quoteTextElement.textContent = randomQuote.text;
+    quoteAuthorElement.textContent = randomQuote.author;
+    }
+}
+
+fetch("https://type.fit/api/quotes")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    const quotes = {};
+
+    data.forEach(function(quote, index) {
+    const text = quote.text;
+    const author = quote.author;
+
+    quotes[index] = {
+        text: text,
+        author: author
+    };
+    });
+    updateQuote(quotes);
+});
