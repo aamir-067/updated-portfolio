1cfc72afa19dd98250c8431dafda2c133687df9d
aamir <aamirkhan@engineer.com>
2023-07-18 13:58:41 +0500

    added testimonials section which needs modifications and another minor changes 😫

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # updated-protfolio
 
 there are some images in the picture folder which is not yet used but i stored it for the future use so dont delete it thanks.
+
+<!-- note for myself -->
+<!--  dont commit this  -->
+
+practice branch : has testim changes which is not yet completed
+main branch : has other changes which needs some modifications but it will ne completed.
--- script.js
+++ script.js
@@ -32,36 +32,96 @@ fetch("https://type.fit/api/quotes")
 
 
 // code for projects section
-(function () {
-    var carousels = function () {
-        $(".owl-carousel1").owlCarousel({
-            loop: true,
-            center: true,
-            margin: 0,
-            responsiveClass: true,
-            nav: false,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false
-                },
-                680: {
-                    items: 2,
-                    nav: false,
-                    loop: false
-                },
-                1000: {
-                    items: 3,
-                    nav: true
-                }
-            }
-        });
-    };
+// vars
+var testim = document.getElementById("testim"),
+    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+    touchStartPos,
+    touchEndPos,
+    touchPosDiff,
+    ignoreTouch = 30;
+;
+window.onload = function () {
 
-    (function ($) {
-        carousels();
-    })(jQuery);
-})();
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length - 1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function () {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function () {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function () {
+        playSlide(currentSlide += 1);
+    })
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function () {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function (e) {
+        if (e.key == 'ArrowLeft') {
+            testimLeftArrow.click();
+        }
+        if (e.key == 'ArrowRight') {
+            testimRightArrow.click();
+        }
+    })
+
+    testim.addEventListener("touchstart", function (e) {
+        touchStartPos = e.changedTouches[0].clientX;
+    })
+
+    testim.addEventListener("touchend", function (e) {
+        touchEndPos = e.changedTouches[0].clientX;
+        touchPosDiff = touchStartPos - touchEndPos;
+
+        if (touchPosDiff > 0 + ignoreTouch) {
+            testimLeftArrow.click();
+        } else if (touchPosDiff < 0 - ignoreTouch) {
+            testimRightArrow.click();
+        } else {
+            return;
+        }
+    })
+}
 
 
 
